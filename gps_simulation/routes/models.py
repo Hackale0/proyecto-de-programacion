ffa68ed92dd6c842d4c70fe2b65b9bbb206ab2bb
Martin <martin.cabana@itscordoba.edu.ar>
2024-10-25 16:18:27 -0300

    Se agrego ala clase city: puntos_interes,turismo_info,actividades

--- gps_simulation/routes/models.py
+++ gps_simulation/routes/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=100)
+    puntos_interes = models.TextField()  # Puntos turísticos relevantes
+    turismo_info = models.TextField(blank=True, null=True)
+    img_url = models.URLField(blank=True, null=True)  
+    actividades = models.TextField(blank=True, null=True)  # Actividades disponibles en la ciudad
+
 
     def __str__(self):
         return self.name
